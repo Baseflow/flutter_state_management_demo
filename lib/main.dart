6a29a23a65ce8c408e0a4cbbf11c0c83c0725f8a
Jeroen Weener <JeroenWeener@users.noreply.github.com>
2023-03-28 13:48:33 +0200

    Add todo list

--- lib/main.dart
+++ lib/main.dart
@@ -14,25 +14,34 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  MyHomePage({super.key});
+
+  final List<String> _todoItems = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: const <Widget>[
-            TextField(),
-            Text('Homepage'),
-          ],
+        child: Expanded(
+          child: ListView.builder(
+            itemCount: _todoItems.length,
+            itemBuilder: (BuildContext context, int index) => ListTile(
+              title: Text(_todoItems[index]),
+            ),
+          ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(onPressed: _addTodoItem),
     );
+  }
+
+  void _addTodoItem() {
+    _todoItems.add('Placeholder');
   }
 }
