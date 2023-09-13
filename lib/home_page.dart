8fe8aee8a8791cc57a057320086647245c764985
Rick van Dijk <rickavdijk@gmail.com>
2023-09-13 21:39:52 +0200

    added todo items, fab and listview

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({super.key})
+      : _todoItems = [
+          'Item 1',
+          'Item 2',
+          'Item 3',
+        ];
+
+  final List<String> _todoItems;
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +16,21 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: const Center(
-        child: SizedBox.shrink(),
+      body: ListView.builder(
+        itemCount: _todoItems.length,
+        itemBuilder: (BuildContext context, int index) {
+          final todo = _todoItems[index];
+          return ListTile(
+            title: Text(todo),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _todoItems.add('Item ${_todoItems.length + 1}');
+        },
+        tooltip: 'Add Todo',
+        child: const Icon(Icons.add),
       ),
     );
   }
