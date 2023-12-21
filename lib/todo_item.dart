51394fb86cd68fe0de940a02f3a0983e86ce859a
Rick van Dijk <rickavdijk@gmail.com>
2023-12-21 13:51:20 +0100

    created exercise 2 v2

--- lib/todo_item.dart
+++ lib/todo_item.dart
@@ -1,8 +1,5 @@
-/// The model for a todo item.
 class TodoItem {
-  /// Creates a new [TodoItem].
   TodoItem({required this.title});
 
-  /// Describes the task to be done.
   final String title;
 }
