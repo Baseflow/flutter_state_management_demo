10924fc80a88da8829e896e932b698d8b5b7228b
Rick van Dijk <rickavdijk@gmail.com>
2023-12-21 13:36:46 +0100

    created first exercise v2

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_state_management_demo/todo_item.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key})
-      : _todoItems = [
-          'Item 1',
-          'Item 2',
-          'Item 3',
-        ];
+      : _controller = TextEditingController(),
+        _todoItems = const [];
 
-  final List<String> _todoItems;
+  final List<TodoItem> _todoItems;
+  final TextEditingController _controller;
 
   @override
   Widget build(BuildContext context) {
@@ -16,21 +15,31 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: ListView.builder(
-        itemCount: _todoItems.length,
-        itemBuilder: (BuildContext context, int index) {
-          final todo = _todoItems[index];
-          return ListTile(
-            title: Text(todo),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _todoItems.add('Item ${_todoItems.length + 1}');
-        },
-        tooltip: 'Add Todo',
-        child: const Icon(Icons.add),
+      body: Column(
+        children: [
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(
+              hintText: 'Enter a new todo item',
+              contentPadding: EdgeInsets.all(16),
+            ),
+            onSubmitted: (value) {
+              // TODO: Add the value to the list of items;
+              _controller.clear();
+            },
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _todoItems.length,
+              itemBuilder: (BuildContext context, int index) {
+                final todo = _todoItems[index];
+                return ListTile(
+                  title: Text(todo.title),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/todo_item.dart
@@ -0,0 +1,8 @@
+/// The model for a todo item.
+class TodoItem {
+  /// Creates a new [TodoItem].
+  TodoItem({required this.title});
+
+  /// Describes the task to be done.
+  final String title;
+}
